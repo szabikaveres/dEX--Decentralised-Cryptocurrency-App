6eac596c8dd40a9e13cf2b30f276d251eb1c3440
User <user@Users-MacBook-Air.local>
2023-06-08 11:39:48 +0300

    Token Transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -12,15 +12,39 @@ contract Token{
     //Track Balances
     mapping(address => uint256) public balanceOf;
 
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 value);
 
     //Send Tokens
+    constructor(
+        string memory _name, 
+        string memory _symbol, 
+        uint256 _totalSupply
+        ) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply= _totalSupply * (10**decimals);
+        balanceOf[msg.sender] = totalSupply;
+        }
 
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
-    name = _name;
-    symbol = _symbol;
-    totalSupply= _totalSupply * (10**decimals);
-    balanceOf[msg.sender] = totalSupply;
-    }
+    function transfer(address _to, uint256 _value) 
+        public 
+        returns (bool success) 
+        {
 
+        require(balanceOf[msg.sender] >= _value);
+        require(_to != address(0));
+
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+
+        balanceOf[_to] = balanceOf[_to] + _value;
+        
+        emit Transfer(msg.sender, _to, _value);
+   
+        return true;
+        
+        }
 }
 
