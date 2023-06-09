21f912f87cb8f536aec8e947cbd291ca73d81fe2
User <user@Users-MacBook-Air.local>
2023-06-09 10:25:14 +0300

    Token Approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,50 +1,65 @@
-// SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract Token{
+contract Token {
     string public name;
     string public symbol;
     uint256 public decimals = 18;
-    uint256 public totalSupply; 
+    uint256 public totalSupply;
 
-    //Track Balances
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(
         address indexed from,
         address indexed to,
-        uint256 value);
+        uint256 value
+    );
 
-    //Send Tokens
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+
     constructor(
-        string memory _name, 
-        string memory _symbol, 
+        string memory _name,
+        string memory _symbol,
         uint256 _totalSupply
-        ) {
+    ) {
         name = _name;
         symbol = _symbol;
-        totalSupply= _totalSupply * (10**decimals);
+        totalSupply = _totalSupply * (10**decimals);
         balanceOf[msg.sender] = totalSupply;
-        }
+    }
 
-    function transfer(address _to, uint256 _value) 
-        public 
-        returns (bool success) 
-        {
-
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
         require(balanceOf[msg.sender] >= _value);
         require(_to != address(0));
 
         balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-
         balanceOf[_to] = balanceOf[_to] + _value;
-        
-        emit Transfer(msg.sender, _to, _value);
-   
-        return true;
-        
-        }
-}
 
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns(bool success)
+    {
+        require(_spender != address(0));
+
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+}
